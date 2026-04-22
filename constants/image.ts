eb6edc21b821f619f363dea97b978a8dfbc0e885
Samuel Oluwatobi Oluwasegun <oluwasegunsam56@gmail.com>
2026-04-22 21:57:18 +0100

    Add new brand and logo images; remove unused SVG files
    - Added brand.jpg, primary-logo.png, and secondary-logo.jpg to the assets/images directory.
    - Deleted obsolete SVG files: file.svg, globe.svg, next.svg, vercel.svg, and window.svg.

---
+++ constants/image.ts
@@ -0,0 +1,9 @@
+import brand from '../public/assets/images/brand.jpg';
+import primary_logo from '../public/assets/images/primary-logo.png';
+import secondary_logo from '../public/assets/images/secondary-logo.jpg';
+
+export const IMAGES = {
+  PRIMARY_LOGO: primary_logo,
+  SECONDARY_LOGO: secondary_logo,
+  BRAND: brand,
+};
