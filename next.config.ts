3c3eb0aec0513869729879098d94beec948cdf9e
Samuel Oluwatobi Oluwasegun <oluwasegunsam56@gmail.com>
2026-04-24 13:13:57 +0100

    feat: implement shop page structure with internationalized content and localized routing

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,18 @@ const withNextIntl = createNextIntlPlugin('./i18n/request.ts');
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+      {
+        protocol: 'http',
+        hostname: '**',
+      },
+    ],
+  },
 };
 
 export default withNextIntl(nextConfig);
