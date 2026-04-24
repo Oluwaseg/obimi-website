2e2f57e747f259ca27ed6538218a9b2e418eeadb
Samuel Oluwatobi Oluwasegun <oluwasegunsam56@gmail.com>
2026-04-24 11:29:20 +0100

    feat: add team member profile images and update constants for image assets

--- constants/image.ts
+++ constants/image.ts
@@ -2,8 +2,24 @@ import brand from '../public/assets/images/brand.jpg';
 import primary_logo from '../public/assets/images/primary-logo.png';
 import secondary_logo from '../public/assets/images/secondary-logo.png';
 
+// Teams
+
+// Parent Support & Community Volunteer
+import tatum from '../public/assets/images/tatum-haslop.jpeg';
+
+// Founder & Director
+import helen2 from '../public/assets/images/helen-ewumi-2.jpeg';
+import helen from '../public/assets/images/helen-ewumi.jpeg';
+
+// Social Media Manager
+import funmi from '../public/assets/images/funmi.jpeg';
+
 export const IMAGES = {
   PRIMARY_LOGO: primary_logo,
   SECONDARY_LOGO: secondary_logo,
   BRAND: brand,
+  TATUM_HASLOP: tatum,
+  HELEN_EWUMI: helen,
+  HELEN_EWUMI_2: helen2,
+  FUNMI: funmi,
 };
